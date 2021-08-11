c990de064d27c5e001083293a1431a5640f52cc7
aliusa93 <84295361+aliusa93@users.noreply.github.com>
2021-08-11 13:07:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js')
 const client = new Discord.Client({
 	intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
 })
-const config = require('./config.json')
 client.on('ready', () => {
 	console.log('Ready')
 })
