b449cde0f1ccba80ffe8f2769d8ff47129aa2c70
aliusa <aliusa9809@gmail.com>
2021-08-22 10:22:48 -0400

    a

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId),
+			Routes.applicationCommands(clientId),
 			{ body: slashCommands },
 		);
 
